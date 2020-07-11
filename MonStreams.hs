c64080205f9ffa7a6ba2e174e16ecb389f000f8d
Venanzio Capretta <venanzio@duplavis.com>
2020-07-11 11:35:09 +0100

    Functor/Applicative/Monad instances for MonStr
    I separated the examples (instantiations with concrete monads)
    to a separate file MonStrExamples
    
    In MonStreams I defined instances of Functor, Applicative, Monad
    I experimented with the lazy list instantiation.
    
    The Monad instance is still incorrect:
      joinMS should give the diagonal, but doesn't

--- MonStreams.hs
+++ MonStreams.hs
@@ -1,144 +1,41 @@
 -- Monadic Streams
 --   Venanzio Capretta, 2020
 
-import Stream
-import Control.Monad.State
-import System.IO.Unsafe
-import Control.Concurrent
-import Control.Concurrent.Async
+module MonStreams where
+
+import Control.Applicative
+import Control.Monad
 
 -- Type of monadic streams
 -- f is not required to be a monad
 
 data MonStr m a = MCons (m (a , MonStr m a))
 
+headMS :: Functor m => MonStr m a -> m a
+headMS (MCons m) = fmap fst m
+
+tailMS :: Functor m => MonStr m a -> m (MonStr m a)
+tailMS (MCons m) = fmap snd m
+
 -- MonStr class instances and helper functions
 ----------------------------------------------
 
+instance Functor m => Functor (MonStr m) where
+  -- fmap :: (a -> b) -> MonStr m a -> MonStr m b
+  fmap f (MCons m) = MCons $ fmap (\(a,s) -> (f a, fmap f s)) m
 
--- Example: Lazy lists - Maybe Monad
-------------------------------------
+instance Applicative m => Applicative (MonStr m) where
+  -- pure :: a -> MonStr m a
+  pure a = MCons $ pure (a, pure a)
 
-type LList a = MonStr Maybe a
+  -- (<*>) :: MonStr m (a->b) -> MonStr m a -> MonStr m b
+  MCons fs <*> MCons as = MCons $
+    (\(f,fs') -> \(a,as') -> (f a, fs' <*> as')) <$> fs <*> as
 
-nil :: LList a
-nil = MCons Nothing
+joinMS :: Monad m => MonStr m (MonStr m a) -> MonStr m a
+joinMS ss = MCons $ (,) <$> join (fmap headMS (headMS ss))
+                        <*> fmap joinMS (join (fmap tailMS (tailMS ss)))
 
-cons :: a -> LList a -> LList a
-cons a l = MCons (Just (a,l))
-
-llist :: [a] -> LList a
-llist = foldr cons nil
-
-fromL :: LList a -> [a]
-fromL (MCons Nothing) = []
-fromL (MCons (Just (a,l))) = a : fromL l
-
--- Arbitrarily branching trees - List Monad
--------------------------------------------
-
-type Tree a = MonStr [] a
-
-leaf :: Tree a
-leaf = MCons []
-
-node :: [(a,Tree a)] -> Tree a
-node l = MCons l
-
--- Depth-first traversal
-dfLabels :: Tree a -> [a]
-dfLabels (MCons l) =  concat (Prelude.map (\(a,l') -> a:dfLabels l') l)
-
--- Breath-first traversal
-bfLabels :: Tree a -> [a]
-bfLabels t = bfLabs [t]
-
-bfLabs :: [Tree a] -> [a]
-bfLabs [] = []
-bfLabs ((MCons l):ts) = Prelude.map fst l ++ bfLabs (ts ++ Prelude.map snd l)
-
-
-t1 = node [(5, leaf),
-           (3, node [(1, leaf)]),
-           (2, node [])
-          ]
-
--- Interactive Processes -- IO Monad
-------------------------------------
-
-type Process a = MonStr IO a
-
-runProcess :: Process a -> IO [a]
-runProcess (MCons s) = do
-    (a,s') <- s
-    as <- runProcess s'
-    return (a:as)
-
--- Runs the process using unsafeInterleaveIO - this allows each IO
---  operation to be run at any time, allowing for lazy evaluation
-unsafeRunProcess :: Process a -> IO [a]
-unsafeRunProcess (MCons s) = do
-    (a,s') <- s
-    as <- unsafeInterleaveIO (unsafeRunProcess s')
-    return (a:as)
-
--- A stream that adds up the inputs from the user
--- Prints the partial sums
-
-sumProc :: Int -> Process Int
-sumProc n = MCons $ do
-  putStrLn ("sum so far: " ++ (show n))
-  s <- getLine
-  let n' = n + read s
-  return (n, sumProc n')
-
--- How to use the (infinite) return of a process
---  stop when it is zero
-
--- This stops after the next input from the user
-stopAtZero :: Process Int -> IO [Int]
-stopAtZero (MCons s) = do
-     (n, s') <- s
-     ns <- (if (n == 0) then (pure []) else stopAtZero s')
-     return (n:ns)
-
-stopAtZero' :: Process Int -> IO [Int]
-stopAtZero' s = do
-     ns <- unsafeRunProcess s
-     return $! Prelude.takeWhile (/= 0) ns
-
--- But stopAtZero (sumProc 0) doesn't stop when the sum reaches 0
---   s not lazily evaluated?
-
--- Trying to find a way to make the output wait before the program is
---  finished
--- main :: IO ()
--- main = do
---   ret <- concurrently (stopAtZero' $ sumProc 5) (return 1)
---   print (ret :: ([Int], Int))
-
-
--- State Monad
---------------
-
-type StatefulStream s a = MonStr (State s) a
-
--- collapseStateStream :: StatefulStream s a -> State s a
--- collapseStateStream (MCons sts) s = do
---                                       let ((a, sts'), s') = runState sts s
---                                       sts >>= collapseStateStream sts' s'
-
-runStr :: StatefulStream s a -> s -> Stream a
-runStr (MCons sts) s = let ((a, sts'), s') = runState sts s
-                       in a <:> (runStr sts' s')
-                       
--- generates a stream flipping between 1 and 0
-flipper :: StatefulStream Int Int
-flipper = MCons (state (\n -> ((n, flipper), (n+1) `mod` 2)))
-
--- a stream that generates the fibonnacci numbers
-fibGen :: StatefulStream (Int, Int) Int
-fibGen = MCons (state (\(a, b) -> ((b, fibGen), (b, a + b))))
-
-fib :: Stream Int
-fib = runStr fibGen (0, 1)
+instance Monad m => Monad (MonStr m) where
+  -- (>>=) :: MonStr m a -> (a -> MonStr m b) -> MonStr m b
+  as >>= f = joinMS (fmap f as)
