9af3c118b289a2e6fe2d2f3222a27a7de6a806b2
Venanzio Capretta <venanzio@duplavis.com>
2020-07-17 11:47:13 +0100

    Definition of pure functions

--- Morphisms.hs
+++ Morphisms.hs
@@ -9,3 +9,6 @@ type MMorphism m0 m1 = forall a. m0 a -> m1 a
 morphMS :: (Functor m0, Functor m1) => MMorphism m0 m1 -> MonStr m0 a -> MonStr m1 a
 morphMS phi (MCons m) = MCons (phi $ fmap (\(a,s) -> (a,morphMS phi s)) m)
                                      
+
+type PureFun a b = forall m. MonStr m a -> m b
+
