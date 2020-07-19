05ec31cd71faf73df2d0ef987a99808ee5382cb4
Venanzio Capretta <venanzio@duplavis.com>
2020-07-19 12:10:09 +0100

    More readable instances of Functor and Applicative
    I devined several notations to make the definitions a bit more natural:
    <:, <::, <:::  for infox MCons with arguments with or without m in front

--- MonStreams.hs
+++ MonStreams.hs
@@ -11,26 +11,78 @@ import Control.Monad
 
 data MonStr m a = MCons (m (a , MonStr m a))
 
-headMS :: Functor m => MonStr m a -> m a
-headMS (MCons m) = fmap fst m
+-- Terminology: "monster" means monadic stream, an element of: MonStr m a
+--  m-monster means an monadic stream under the monad, an element of: m (MonStr m a)
 
-tailMS :: Functor m => MonStr m a -> m (MonStr m a)
-tailMS (MCons m) = fmap snd m
+unwrapMS :: MonStr m a -> m (a, MonStr m a)
+unwrapMS (MCons m) = m
+
+
+{- Infix notation for monadic streams:
+   We use (<:) for "pure cons": appending a pure element in front of a monster
+     (<:) :: a -> MonStr m a -> Monstr m a        -- only for m Applicative
+
+   We add an extra colon when one of the argument is under m:
+     (<::) :: m a -> Monstr m a -> Monstr m a     -- for m Functor
+     (<:::) :: m a -> m Monstr m a -> Monstr m a  -- for m Applicative
+-}
+
 
 -- MonStr class instances and helper functions
 ----------------------------------------------
 
+-- Operations for m Functor
+
+headMS :: Functor m => MonStr m a -> m a
+headMS = fmap fst . unwrapMS
+
+tailMS :: Functor m => MonStr m a -> m (MonStr m a)
+tailMS = fmap snd . unwrapMS
+
+-- Appending an m-element in front of a stream
+infixr 5 <::
+(<::) :: (Functor m) => m a -> MonStr m a -> MonStr m a
+ma <:: s = MCons (fmap (\a -> (a,s)) ma)
+
 instance Functor m => Functor (MonStr m) where
   -- fmap :: (a -> b) -> MonStr m a -> MonStr m b
-  fmap f (MCons m) = MCons $ fmap (\(a,s) -> (f a, fmap f s)) m
+  fmap f = MCons . fmap (\(a,s) -> (f a, fmap f s)) . unwrapMS
+
+
+-- Operations for m Applicative
+
+-- Generic pairing operation for Applicatives
+pairA :: Applicative m => m a -> m b -> m (a,b)
+pairA = liftA2 (,)
+
+-- appending an m-element in front of an m-stream
+infix 5 <:::
+(<:::) :: Applicative m => m a -> m (MonStr m a) -> MonStr m a
+ma <::: ms = MCons (pairA ma ms)
+
+-- Appends a pure element to the beginning of the given monadic stream
+infixr 5 <:
+(<:) :: (Applicative m) => a -> MonStr m a -> MonStr m a
+a <: s = pure a <:: s
+
+-- Double Applicative
+infix 5 <<*>>
+(<<*>>) :: (Applicative m1, Applicative m2) =>
+           m1 (m2 (a->b)) -> m1 (m2 a) -> m1 (m2 b)
+(<<*>>) = liftA2 (<*>)
 
 instance Applicative m => Applicative (MonStr m) where
   -- pure :: a -> MonStr m a
-  pure a = MCons $ pure (a, pure a)
+  pure a = a <: pure a -- constant stream
 
   -- (<*>) :: MonStr m (a->b) -> MonStr m a -> MonStr m b
-  MCons fs <*> MCons as = MCons $
-    (\(f,fs') -> \(a,as') -> (f a, fs' <*> as')) <$> fs <*> as
+  fs <*> as = (headMS fs <*> headMS as) <::: (tailMS fs <<*>> tailMS as)
+    -- (headMS fs <*> headMS as) <:: (tailMS fs <*> tailMS as) 
+
+
+
+
+
 
 -- "lift" monadic actions from the elements to the stream
 liftMS :: Monad m => MonStr m (m a) -> m (MonStr m a)
