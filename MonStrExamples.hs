07a41852e3d535214811f9e33d42373ff73a7a6c
Venanzio Capretta <venanzio@duplavis.com>
2020-07-30 11:29:12 +0100

    Concatenation of two monsters
    I created a new module Operations
    goal: reimplement the functions from Data.List and Data.Stream for monsters
    whenever possible: may require some assumptions on m
    
    For now implemented concatenation (+++): m must be an instance of Alternative
    
    I also added the Examples file (I had forgotten to add it previously)

---
+++ MonStrExamples.hs
@@ -0,0 +1,169 @@
+{-
+  Monadic Streams
+  Instantiations with various monads
+-}
+
+module MonStrExamples where
+
+import MonStreams
+import PureStreams
+import Operations
+
+import Control.Monad.State
+import System.IO.Unsafe
+
+
+-- Example: Lazy lists - Maybe Monad
+------------------------------------
+
+type LList a = MonStr Maybe a
+
+nil :: LList a
+nil = MCons Nothing
+
+cons :: a -> LList a -> LList a
+cons a l = MCons (Just (a,l))
+
+llist :: [a] -> LList a
+llist = foldr cons nil
+
+fromL :: LList a -> [a]
+fromL (MCons Nothing) = []
+fromL (MCons (Just (a,l))) = a : fromL l
+
+
+-- Examples to test the correctness of the Monad instantiation:
+--   verify that joinMS takes the diagonal
+
+-- taking a submatrix of size n x m
+subMatrix :: [[a]] -> Int -> Int -> [[a]]
+subMatrix xss n m = Prelude.take n (fmap (Prelude.take m) xss)
+
+toMM :: [[a]] -> MonMatrix Maybe a
+toMM = llist . fmap llist
+
+toMatrix :: MonMatrix Maybe a -> [[a]]
+toMatrix = fromL . fmap fromL
+
+-- list of lists of coordinates
+xys :: [[(Int,Int)]]
+xys = [[(x,y) | y <- [0..] ] | x <- [0..] ]
+
+xyLL :: MonMatrix Maybe (Int,Int)
+xyLL = llist (fmap llist xys)
+
+-- fromL $ joinMS xyLL
+--   correctly gives the diagonal: (0,0),(1,1),(2,2),(3,3),..]
+
+
+
+
+-- Arbitrarily branching trees - List Monad
+-------------------------------------------
+
+type Tree a = MonStr [] a
+
+leaf :: Tree a
+leaf = MCons []
+
+node :: [(a,Tree a)] -> Tree a
+node l = MCons l
+
+-- Depth-first traversal
+dfLabels :: Tree a -> [a]
+dfLabels (MCons l) =  concat (Prelude.map (\(a,l') -> a:dfLabels l') l)
+
+-- Breath-first traversal
+bfLabels :: Tree a -> [a]
+bfLabels t = bfLabs [t]
+
+bfLabs :: [Tree a] -> [a]
+bfLabs [] = []
+bfLabs ((MCons l):ts) = Prelude.map fst l ++ bfLabs (ts ++ Prelude.map snd l)
+
+
+t1 = node [(5, leaf),
+           (3, node [(1, leaf)]),
+           (2, node [(4, leaf),(7,leaf)])
+          ]
+
+
+-- Interactive Processes -- IO Monad
+------------------------------------
+
+type Process a = MonStr IO a
+
+runProcess :: Process a -> IO [a]
+runProcess (MCons s) = do
+    (a,s') <- s
+    as <- runProcess s'
+    return (a:as)
+
+-- Runs the process using unsafeInterleaveIO - this allows each IO
+--  operation to be run at any time, allowing for lazy evaluation
+unsafeRunProcess :: Process a -> IO [a]
+unsafeRunProcess (MCons s) = do
+    (a,s') <- s
+    as <- unsafeInterleaveIO (unsafeRunProcess s')
+    return (a:as)
+
+-- A stream that adds up the inputs from the user
+-- Prints the partial sums
+
+sumProc :: Int -> Process Int
+sumProc n = MCons $ do
+  putStrLn ("sum so far: " ++ (show n))
+  s <- getLine
+  let n' = n + read s
+  return (n, sumProc n')
+
+-- How to use the (infinite) return of a process
+--  stop when it is zero
+
+-- This stops after the next input from the user
+stopAtZero :: Process Int -> IO [Int]
+stopAtZero (MCons s) = do
+     (n, s') <- s
+     ns <- (if (n == 0) then (pure []) else stopAtZero s')
+     return (n:ns)
+
+stopAtZero' :: Process Int -> IO [Int]
+stopAtZero' s = do
+     ns <- unsafeRunProcess s
+     return $! Prelude.takeWhile (/= 0) ns
+
+-- But stopAtZero (sumProc 0) doesn't stop when the sum reaches 0
+--   s not lazily evaluated?
+
+-- Trying to find a way to make the output wait before the program is
+--  finished
+-- main :: IO ()
+-- main = do
+--   ret <- concurrently (stopAtZero' $ sumProc 5) (return 1)
+--   print (ret :: ([Int], Int))
+
+
+-- State Monad
+--------------
+
+type StatefulStream s a = MonStr (State s) a
+
+-- collapseStateStream :: StatefulStream s a -> State s a
+-- collapseStateStream (MCons sts) s = do
+--                                       let ((a, sts'), s') = runState sts s
+--                                       sts >>= collapseStateStream sts' s'
+
+runStr :: StatefulStream s a -> s -> Stream a
+runStr (MCons sts) s = let ((a, sts'), s') = runState sts s
+                       in a <: (runStr sts' s')
+                       
+-- generates a stream flipping between 1 and 0
+flipper :: StatefulStream Int Int
+flipper = MCons (state (\n -> ((n, flipper), (n+1) `mod` 2)))
+
+-- a stream that generates the fibonnacci numbers
+fibGen :: StatefulStream (Int, Int) Int
+fibGen = MCons (state (\(a, b) -> ((b, fibGen), (b, a + b))))
+
+fib :: Stream Int
+fib = runStr fibGen (0, 1)
---
+++ Operations.hs
@@ -0,0 +1,32 @@
+{- Operations on Monadic Streams
+   Reimplementation of the functions in Data.List
+   Generalization of Data.Stream (Swiestra and van Dijk)
+
+  Notation and terminology:
+  We use the same names as for the equivalent functions in Data.List
+  with an -MS postfix.
+  In general the result of the operation will be inside the monad;
+  in cases in which it is 'pure', the postfix becomes -MMS
+  Example (from MonStream.hs):
+    tailMS  :: MonStr m a -> m (MonStr m a)   works for m Functor
+    tailMMS :: MonStr m a -> MonStr m a       works for m Applicative
+-}
+
+module Operations where
+
+import MonStreams
+
+import Control.Applicative
+
+instance Alternative m => Alternative (MonStr m) where
+  -- empty :: MonStr m a
+  empty = MCons empty
+
+  -- (<|>) :: MonStr m a -> MonStr m a -> MonStr m a
+  (MCons m1) <|> (MCons m2) = MCons (m1 <|> m2)
+  
+
+-- Appending two monsters: the second is grafted when there is an empty action
+infixr 5 +++
+(+++) :: Alternative m => MonStr m a -> MonStr m a -> MonStr m a
+s1 +++ s2 = (headMS s1 <::: ((+++) <$> tailMS s1 <*> pure s2)) <|> s2
