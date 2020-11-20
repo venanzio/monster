e9c57c0ed29b62847894a09f6c7eb3a5c2b3f1b5
psycjpu <psycjpu@exmail.nottingham.ac.uk>
2020-11-20 14:23:27 +0000

    Added blocks to break in breakoutHGL
    Need a *much* better collision detection and resolution calulation

--- interactive_demo/BreakoutHGL.hs
+++ interactive_demo/BreakoutHGL.hs
@@ -4,6 +4,8 @@ import Operations
 import Combinators
 import MonStrExamples
 import Control.Monad.State
+import Data.List
+import Data.Maybe
 import Graphics.HGL
 
 {-
@@ -14,7 +16,7 @@ Dependancies: HGL-3.2.3.2 (cabal install HGL --lib, and then add "package-id HGL
 
 -}
 
-type GameState = (Int, (Double, Double), (Double, Double), Window)
+type GameState = (Int, (Double, Double), (Double, Double), [(Double,Double)], Window)
 
 data ArrowKey = U | D | L | R deriving Show
 
@@ -25,7 +27,7 @@ screenHeight :: Int
 screenHeight = 240
 
 ballSpeed :: Double
-ballSpeed = 0.6
+ballSpeed = 1.0
 
 getMaybeKeyPress :: Window -> IO (Maybe ArrowKey)
 getMaybeKeyPress w = do e <- maybeGetWindowEvent w
@@ -39,13 +41,13 @@ getMaybeKeyPress w = do e <- maybeGetWindowEvent w
                                 )
 
 movePaddle :: StateT GameState IO ()
-movePaddle = do (batp, ballp, ballv, w) <- get
+movePaddle = do (batp, ballp, ballv, blockps, w) <- get
                 (arrow,_) <- lift $ par (getMaybeKeyPress w) (getWindowTick w)
                 case arrow of
                    Nothing -> return ()
                    Just a -> case a of
-                                L -> if batp == 0 then return () else put ((batp - 5), ballp, ballv, w)
-                                R -> if batp == 730 then return () else put ((batp + 5), ballp, ballv, w)
+                                L -> if batp == 0 then return () else put ((batp - 5), ballp, ballv, blockps, w)
+                                R -> if batp == 730 then return () else put ((batp + 5), ballp, ballv, blockps, w)
                                 _ -> return ()
 
 constrainBallY :: (Double,Double) -> (Double, Double) -> ((Double,Double),(Double, Double))
@@ -58,27 +60,53 @@ constrainBall (bx,by) (bvx,bvy) = if bx <= 0 then constrainBallY (0,by) (-bvx,bv
                                              else if bx >= fromIntegral screenWidth then constrainBallY ((fromIntegral screenWidth)-0.1,by) (-bvx,bvy)
                                                                                     else constrainBallY (bx,by) (bvx,bvy)
 
+calculateBounce :: Double -> Double -> Double -> (Double, Double)
+calculateBounce bx cx width = (ballSpeed * sin (a*offset), (-ballSpeed) * cos (a*offset))
+                              where offset = negate $ ((cx + (width/2.0)) - bx)/(cx + (width/2.0))
+                                    a      = 2 * pi
+
 batCollision :: GameState -> GameState
-batCollision r@(batp, (bx,by), (bvx,bvy), w) = if (round by == 220) && (signum(bvy) > 0) then 
-                                                 (if bx >= fromIntegral (batp-10) && bx < fromIntegral (batp+70) then (batp, (bx,by), (((2.0/70.0) * (bx - fromIntegral batp)) - 1,-bvy),w)
-                                                                                                           else r) 
-                                                                                       else r
-                           
+batCollision r@(batp, (bx,by), (bvx,bvy), blockps, w) = if (round by > 220 && round by <= 230) && (signum (bvy) > 0) then 
+                                                        (if bx >= fromIntegral (batp-10) && bx < fromIntegral (batp+70) then (batp, (bx,220), calculateBounce bx (fromIntegral batp) 60.0,blockps,w)
+                                                                                                                        else r) 
+                                                                                                  else r
+
+blockCollisions :: GameState -> GameState
+blockCollisions r@(batp, (bx,by), (bvx,bvy), [], w)          = r
+blockCollisions r@(batp, (bx,by), (bvx,bvy), (b:blockps), w) = case singleBlockCollision (bx,by) b of 
+                                                                  Just (nxv,nyv) -> (batp, (bx,by), (nxv,nyv), blockps, w)
+                                                                  Nothing        -> (\(bp,bap,bav,bs,w) -> (bp,bap,bav,(b:bs),w)) $ blockCollisions (batp, (bx,by), (bvx,bvy), blockps, w)
+                                                                  
+-- Block positions indicate bottom left corner of blocks, and blocks are 20x10
+-- Maybe returns a new ball speed
+singleBlockCollision :: (Double, Double) -> (Double, Double) -> Maybe (Double,Double) 
+singleBlockCollision (bx,by) (blx, bly) = if (bx > blx - 1 && bx < blx + 21) then
+                                             if (by > bly && by < bly + 10) then Just (calculateBounce bx blx 20.0)
+                                                                                else Nothing
+                                                                         else Nothing
+                                           
+
 moveBall :: StateT GameState IO ()
-moveBall = do (batp, ballp, ballv, w) <- get 
+moveBall = do (batp, ballp, ballv, blockps, w) <- get 
               let ((bx,by), (bvx,bvy)) = constrainBall ballp ballv 
-                  (batp', (bx',by'), (bvx',bvy'), _) = batCollision (batp, (bx,by), (bvx,bvy), w) in
-                  put (batp', (bx' + (ballSpeed * bvx'), by' + (ballSpeed * bvy')), (bvx',bvy'), w)
+                  -- Interestingly, reassigning w here (where the _ is), breaks the window pointer
+                  (batp', (bx',by'), (bvx',bvy'), blockps', _) = (blockCollisions . batCollision) (batp, (bx,by), (bvx,bvy), blockps, w) in
+                  put (batp', (bx' + (ballSpeed * bvx'), by' + (ballSpeed * bvy')), (bvx',bvy'), blockps', w)
+                  
+rectangle :: Point -> Point -> Graphic
+rectangle (x,y) (x',y') = polygon [(x, y),(x,y'),(x',y'),(x',y)]
                   
 printGame :: StateT GameState IO ()
-printGame = do (batp, ballp, ballv, w) <- get
+printGame = do (batp, ballp, ballv, blockps, w) <- get
                lift $ clearWindow w
-               -- printing the ball, y in [0,24), x in [0,80), y = 0 is the top of the screen
+               -- printing the ball, y in [0,240), x in [0,800), y = 0 is the top of the screen
                (let (bx,by) = (\(x,y) -> (round x, round y)) ballp in
                    lift $ drawInWindow w (ellipse (bx - 5, by - 5) (bx + 5, by + 5))
                    )
                -- printing bat
-               lift $ drawInWindow w (polygon [(batp-5, 220),(batp-5,230),(batp+65,230),(batp+65,220)])
+               lift $ drawInWindow w (rectangle (batp-5, 220) (batp+65,230))
+               -- printing blocks
+               lift $ sequence_ $ fmap (\(x,y) -> drawInWindow w $ rectangle (round x, round y) (round $ x+20,round $ y+10)) blockps
 
 -- Using combinators and streamify to turn the actions into a continuous process          
 game :: MonStr (StateT GameState IO) ()
@@ -87,7 +115,7 @@ game = (streamify movePaddle) |:> moveBall |:> printGame
 main :: IO ()
 main = runGraphics $
        do w <- openWindowEx "Breakout" Nothing (screenWidth, screenHeight) DoubleBuffered (Just 1)
-          runVoidProcess (compileST game (screenWidth `div` 2,((fromIntegral screenWidth/2),3.0),(0,1.0), w))
+          runVoidProcess (compileST game (screenWidth `div` 2,((fromIntegral screenWidth/2),3.0),(0,1.0), [(10 + x * 30, 50) | x <- [1..20]], w))
           closeWindow w
        
        
