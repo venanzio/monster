d10de17b79c175d1d07e9be48ada4671ec7141f5
psycjpu <psycjpu@exmail.nottingham.ac.uk>
2021-04-27 23:26:35 +0100

    Fixed some test issues and added some State-monster example code

--- final_code/Examples/StateMachines.hs
+++ final_code/Examples/StateMachines.hs
@@ -9,20 +9,51 @@ import Prelude hiding (head, tail, map, scanl, scanl1,
 import MonadicStream
 import Examples.PureStreams
 import Control.Monad.State
+import Data.Fix
 
 -- | Type of finite state machines
-type FSMachine i o = MonStr ((->) i) o
+type FSMStr i o = MonStr ((->) i) o
 
 
+-- | Correspondance of Reader-monsters with Mealy machines
 
+-- | The type of Mealy machines
+data Mealy st inA outA = Mealy { initState :: st
+                               , transf :: (st , inA) -> (st , outA)
+                               }
 
+data MFunc e a b = MFunc {unFunc :: e -> (b , a)}
 
+-- | Every Mealy machine is a Reader-monster
+mealyToMonStr :: Mealy s i o -> FSMStr i o
+mealyToMonStr (Mealy s tf) = MCons (\e -> let (s', a) = tf (s, e) in (a, mealyToMonStr (Mealy s' tf)))
 
+-- | Every Reader-monster is a Mealy machine
+monStrToMealy :: FSMStr i o -> Mealy (Fix (MFunc i o)) i o
+monStrToMealy (MCons f) = Mealy (aux f) (\(g, e) -> (unFunc (unFix g)) e)
+                          where 
+                             aux :: (e -> (a, MonStr ((->) e) a)) -> Fix (MFunc e a)
+                             aux f = Fix (MFunc (\e -> let (a, g) = f e in (aux (uncons g), a)))
 
+{-
+Mealy (Fix (MFunc i o)) i o ~= Mealy (MonStr ((->) i) o) i o
+
+but it seems more informative to see the type of states of the Mealy machine
+as the type \nu x. e -> (x, a), as it can be conceptually separated from the 
+generality of monadic streams.
+-}
+                            
+-- | Notion of combining Mealy machines, using Reader-monsters
+-- Basically just nested function composition
+composeFSM :: FSMStr a b -> FSMStr b c -> FSMStr a c
+composeFSM (MCons f) (MCons g) = MCons $ \a -> let (b, f') = f a 
+                                                   (c, g') = g b
+                                                   in (c, composeFSM f' g')
+                                           
 
 
 -- | Type of finite state machines with 'feedback loops'
-type FBMachine s a = MonStr (State s) a
+type FBMachine i o = MonStr (State i) o
 
 compileST :: Monad m => MonStr (StateT s m) a -> s -> MonStr m a
 compileST (MCons sts) s = MCons $ do ((a, sts'), s') <- runStateT sts s
