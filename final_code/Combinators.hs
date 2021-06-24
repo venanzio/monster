b1fe9f5a071a0081560cc29f1414864c9d2dc745
psycjpu <psycjpu@nottingham.ac.uk>
2021-06-24 15:56:20 +0100

    Added 'whenever' and 'if then else' combinators

--- final_code/Combinators.hs
+++ final_code/Combinators.hs
@@ -63,6 +63,25 @@ on :: Monad m => MonStr m a -> MonStr m Bool -> MonStr m a
                                         (b, mb') <- mb
                                         return (a, if b then (ma' `on` mb') else ((MCons ma) `on` mb'))
 
+-- | If then else - should the monadic action of the other stream 
+-- be run if its value is not used, or should it be pushed back a
+-- level with tailM?
+ifte :: Monad m => MonStr m Bool -> MonStr m a -> MonStr m a -> MonStr m a
+ifte mb mt mf = MCons $ do (b, mb') <- uncons mb
+                           if b 
+                              then do (t, mt') <- uncons mt
+                                      return (t, ifte mb' mt' (tailM mf))
+                              else do (f, mf') <- uncons mf
+                                      return (f, ifte mb' (tailM mt) mf')
+
+-- | Whenever
+infixr 4 `wvr`
+wvr :: Monad m => MonStr m a -> MonStr m Bool -> MonStr m a
+ma `wvr` mb = MCons $ do (b, mb') <- uncons mb
+                         if b then do (a, ma') <- uncons ma
+                                      return (a, ma' `wvr` mb')
+                              else uncons $ (tailM ma) `wvr` mb'
+
 
 -- | Examples
 
@@ -81,4 +100,10 @@ test2 = test `on` flipS
 
 test3 :: Stream Int
 test3 = test `on` pure True
- 
+
+
+s :: Stream Int
+s = 1 <: 2 <: 3 <: 4 <: (pure 5)
+
+k :: Stream Int
+k = pure 1 `fby` (k *| pure 2)
