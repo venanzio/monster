f2787cf0b0084e14c5ee8eb7222e25673cf0e7cc
Venanzio Capretta <venanzio@duplavis.com>
2021-09-22 09:52:23 +0100

    Coq: abstract formalization of coinduction I found it difficult to prove the theorems about Applicatives. I think it may be easier to do all the proofs at an abstract level instead of using containers. I formalized the abstract notions of coalgebras, coalgebra morphism,   and bisimulation. Next I will formalize the notion of final coalgebra   and use this to model coinductive types (specifically monsters)

---
+++ coq/coinduction.v
@@ -0,0 +1,61 @@
+(* Abstract formalization of coinduction *)
+
+(* Instead of using functors that are syntactically strictly positive (containers)
+   we characterize abstractly the existence of a final coalgebra.
+*)
+
+(* Identity and composition operators (are they not in the library?) *)
+
+Definition id (A:Set): A -> A := fun x => x.
+
+Definition comp {A B C:Set}(g:B->C)(f:A -> B): A -> C :=
+fun x => g (f x).
+
+(* Definition of functor on Set *)
+
+Record Functor: Type:=
+  { fun_obj: Set -> Set;
+    fun_morph: forall {A B:Set}, (A->B) -> fun_obj A -> fun_obj B;
+    fun_id_law: forall (A:Set), fun_morph (id A) = id (fun_obj A);
+    fun_comp_law: forall (A B C:Set)(f:A->B)(g:B->C),
+                    fun_morph (comp g f) = comp (fun_morph g) (fun_morph f)
+  }.
+
+Parameter F: Functor.
+
+(* F-coalgebras and coalgebra morphisms *)
+
+Record Coalgebra: Type :=
+  { elements: Set;
+    coalgebra: elements -> fun_obj F elements
+  }.
+
+Record CoalgMorphism (coalg1 coalg2: Coalgebra): Type :=
+  { morphism: elements coalg1 -> elements coalg2;
+    coalg_morph_law: 
+      comp (coalgebra coalg2) (morphism) = 
+      comp (fun_morph F morphism) (coalgebra coalg1) 
+  }.
+
+(* A relation between sets is modeled by a span
+   Idea: span_rel is a set of "certificates" that elements of A and B are related
+   p:span_rel is evidence that (span_p1 p) and (span_p2 p) are related
+*)
+
+Record Span (A B:Set): Type :=
+  { span_rel: Set;
+    span_p1: span_rel -> A;
+    span_p2: span_rel -> B
+  }.
+
+(* Bisimulation:
+   A bisimulation is a span in the category of coalgebras
+*)
+
+Record Bisimulation (coalg1 coalg2: Coalgebra): Type :=
+  { bisim_rel: Coalgebra;
+    bisim_p1: CoalgMorphism bisim_rel coalg1;
+    bisim_p2: CoalgMorphism bisim_rel coalg2
+  }.
+
+
